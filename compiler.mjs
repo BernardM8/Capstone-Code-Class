5a77bcbf84b7d7f88fe9421f9960fc25a982416c
Bernard M <bernardmulcahy@bellevuecollege.edu>
2022-02-27 23:47:51 -0800

    try * import

--- compiler.mjs
+++ compiler.mjs
@@ -4,7 +4,8 @@
 //import request from "./request";  //was blocked because of a disallowed MIME type (“text/html”).
 //import request from "request";  //Uncaught TypeError: Error resolving module specifier “request”. Relative module specifiers must start with “./”, “../” or “/”.
 
-import {request} from "./node_modules/request/index.js";
+//import {request} from "./node_modules/request/index.js";
+import * as request from "./node_modules/request/index.js";
 
 export class CompilerFeature{
 
