53a19adbf20e4c5ef348ed12d00da4be31443584
Bernard M <bernardmulcahy@bellevuecollege.edu>
2022-05-09 19:03:26 -0700

    7

--- app2.mjs
+++ app2.mjs
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-app.js";
-import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-database.js";
+import { getDatabase, ref, set, onValue, update } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-database.js";
 
 //import { initializeApp } from "firebase/app";
 //import { getDatabase, ref, set, onValue, update } from "firebase/database";
